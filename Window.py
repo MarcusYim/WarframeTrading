7c31a5b3331b2b1e4a920d4aa649870d63ea5c47
Marcus Yim <marcusyim@outlook.com>
2023-09-25 14:41:12 -0700

    Window.py now destroys buttons when they are pressed

--- Window.py
+++ Window.py
@@ -1,14 +1,20 @@
+from functools import partial
+
 from VerticalScrolledFrame import VerticalScrolledFrame
 import tkinter.ttk as ttk
 import tkinter as tk
 
 
-class Window():
-    def __init__(self, master, *args, **kwargs):
+class Window:
+    def __init__(self, master):
         self.frame = VerticalScrolledFrame(master)
         self.frame.pack(expand=True, fill=tk.BOTH)
-        self.label = ttk.Label(master, text="Shrink the window to activate the scrollbar.")
-        self.label.pack()
 
         for i in range(10):
-            ttk.Button(self.frame.interior, text=f"Button {i}").pack(padx=10, pady=5)
+            b = ttk.Button(self.frame.interior, text=f"Button {i}")
+            b.config(command=partial(self.destroy, b))
+            b.pack(padx=10, pady=5)
+
+    def destroy(self, button):
+        print("beans")
+        button.destroy()
